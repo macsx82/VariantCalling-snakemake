6247ea6ab48d695035da7fe33ad7c419899cbc84
macsx82 <macsx82@gmail.com>
2021-10-24 19:46:43 +0200

    fix typo in function to retrieve correctly males and females samples for joint calling

--- rules/functions.py
+++ rules/functions.py
@@ -92,9 +92,9 @@ def call_variants_by_sex(base_path):
 
     return all_samples_intervals
 
-def sample_names_from_interval(wildcards):
-    print(wildcards)
-    interval_filename=wildcards.interval_name
+def sample_names_from_interval(interval_name):
+    print(interval_name)
+    interval_filename=interval_name
     interval_file=os.path.join(*references_abs_path(), config.get("callable_intervals"),interval_filename)
     for line in open(interval_file, 'r'):
         if not(re.match('@', line.strip())):
